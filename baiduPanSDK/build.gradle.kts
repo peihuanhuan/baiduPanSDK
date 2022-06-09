f600d282c41c0010b66cb2a863a12ab14edeef5a
裴欢 <peihuan@meituan.com>
2022-06-10 01:09:09 +0800

    build: 发布

--- baiduPanSDK/build.gradle.kts
+++ baiduPanSDK/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("maven-publish")
-    id("signing")
 
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
@@ -18,12 +17,6 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 repositories {
     mavenCentral()
 }
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 
 
 dependencies {
@@ -50,107 +43,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-val uploadArchives: Upload by tasks
-uploadArchives.apply {
-    repositories {
-        withConvention(MavenRepositoryHandlerConvention::class) {
-            mavenDeployer {
-                // Sign Maven POM
-                beforeDeployment {
-                    signing.signPom(this)
-                }
-
-                val username = if (project.hasProperty("sonatypeUsername")) project.properties["sonatypeUsername"] else System.getenv("sonatypeUsername")
-                val password = if (project.hasProperty("sonatypePassword")) project.properties["sonatypePassword"] else System.getenv("sonatypePassword")
-
-                withGroovyBuilder {
-                    "snapshotRepository"("url" to "https://oss.sonatype.org/content/repositories/snapshots") {
-                        "authentication"("userName" to username, "password" to password)
-                    }
-
-                    "repository"("url" to "https://oss.sonatype.org/service/local/staging/deploy/maven2") {
-                        "authentication"("userName" to username, "password" to password)
-                    }
-                }
-
-                // Maven POM generation
-                pom.project {
-                    withGroovyBuilder {
-                        "name"(projectName)
-                        "artifactId"(base.archivesBaseName.toLowerCase())
-                        "packaging"("jar")
-                        "url"("https://github.com/OpenCubicChunks/CubicChunksConverter")
-                        "description"("Save converter from vanilla Minecraft to cubic chunks")
-
-
-                        "scm" {
-                            "connection"("scm:git:git://github.com/OpenCubicChunks/CubicChunksConverter.git")
-                            "developerConnection"("scm:git:ssh://git@github.com:OpenCubicChunks/CubicChunksConverter.git")
-                            "url"("https://github.com/OpenCubicChunks/CubicChunksConverter")
-                        }
-
-                        "licenses" {
-                            "license" {
-                                "name"("The MIT License")
-                                "url"("http://www.tldrlegal.com/license/mit-license")
-                                "distribution"("repo")
-                            }
-                        }
-
-                        "developers" {
-                            "developer" {
-                                "id"("Barteks2x")
-                                "name"("Barteks2x")
-                            }
-                        }
-
-                        "issueManagement" {
-                            "system"("github")
-                            "url"("https://github.com/OpenCubicChunks/CubicChunksConverter/issues")
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-tasks {
-
-    // TODO prefer the lazy string invoke once https://github.com/gradle/gradle-native/issues/718 is fixed
-    getByName<Upload>("uploadArchives") {
-
-        repositories {
-
-            withConvention(MavenRepositoryHandlerConvention::class) {
-
-                mavenDeployer {
-
-                    withGroovyBuilder {
-                        "repository"("url" to uri("$buildDir/m2/releases"))
-                        "snapshotRepository"("url" to uri("$buildDir/m2/snapshots"))
-                    }
-
-                    pom.project {
-                        withGroovyBuilder {
-                            "parent" {
-                                "groupId"("org.gradle")
-                                "artifactId"("kotlin-dsl")
-                                "version"("1.0")
-                            }
-                            "licenses" {
-                                "license" {
-                                    "name"("The Apache Software License, Version 2.0")
-                                    "url"("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                                    "distribution"("repo")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
